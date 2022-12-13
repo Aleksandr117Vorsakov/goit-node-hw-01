839ffbb0353180ebc14749183519455f515a62dd
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-12-13 16:40:24 +0200

    task node1

---
+++ index.js
@@ -0,0 +1,75 @@
+const contactsOperations = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      try {
+        const contacts = await contactsOperations.listContacts();
+        console.table(contacts);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        break;
+      }
+
+    case "get":
+      try {
+        const contact = await contactsOperations.getContactById(id);
+        if (!contact) {
+          throw new Error(`Contact with id=${id} not found`);
+        }
+        console.table(contact);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        break;
+      }
+
+    case "add":
+      try {
+        const newContact = await contactsOperations.addContact(
+          name,
+          email,
+          phone
+        );
+        console.table(newContact);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        break;
+      }
+
+    case "remove":
+      try {
+        const removeContact = await contactsOperations.removeContact(id);
+        if (!removeContact) {
+          throw new Error(`Contact with id=${id} not found`);
+        }
+        console.table(removeContact);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        break;
+      }
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+(async () => {
+  await invokeAction(argv);
+})();
